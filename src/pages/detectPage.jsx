38834e90e2edeeeb6153f3302c3b8e8bfb7fa359
filipusarif <filipus123.gmil@gmail.com>
2024-05-13 19:54:45 +0700

    style: edit command/docs and word line

--- src/pages/detectPage.jsx
+++ src/pages/detectPage.jsx
@@ -5,23 +5,25 @@ import Webcam from 'react-webcam';
 import { drawRect } from '../utilities';
 
 export default function Index() {
-
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
 
-    // const [detections, setDetections] = useState([]);
     const [isFrontCamera, setIsFrontCamera] = useState(true);
 
+    // Change Camera Function
     const switchCamera = () => {
         setIsFrontCamera(!isFrontCamera);
     };
-     // Main function
+
+    const videoConstraints = {
+        facingMode: isFrontCamera ? "user" : "environment"
+    };
+
+    // Main function
     const runCoco = async () => {
-        // 3. TODO - Load network 
-        // e.g. const net = await cocossd.load();
         const net = await cocoSsd.load();
         console.log("Dataset Load");
-        //  deteksi 
+        //  Detect Object
         setInterval(() => {
             detect(net);
         }, 1);
@@ -35,92 +37,77 @@ export default function Index() {
         webcamRef.current !== null &&
         webcamRef.current.video.readyState === 4
         ) {
-        // Get Video Properties
-        const video = webcamRef.current.video;
-        const videoWidth = webcamRef.current.video.videoWidth;
-        const videoHeight = webcamRef.current.video.videoHeight;
-    
-        // Set video width
-        webcamRef.current.video.width = videoWidth;
-        webcamRef.current.video.height = videoHeight;
-    
-        // Set canvas height and width
-        canvasRef.current.width = videoWidth;
-        canvasRef.current.height = videoHeight;
-    
-        // 4. TODO - Make Detections
-        // e.g. const obj = await net.detect(video);
-        const obj = await net.detect(video);
-        // console.log(obj);
+            // Get Video Properties
+            const video = webcamRef.current.video;
+            const videoWidth = webcamRef.current.video.videoWidth;
+            const videoHeight = webcamRef.current.video.videoHeight;
         
-    
-        // Draw mesh
-        const ctx = canvasRef.current.getContext("2d");
+            // Set video width
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
+        
+            // Set canvas height and width
+            canvasRef.current.width = videoWidth;
+            canvasRef.current.height = videoHeight;
+        
+            const obj = await net.detect(video);
             
-        // 5. TODO - Update drawing utility
-        // drawSomething(obj, ctx)  
-        // Hitung dan simpan jarak untuk setiap objek
-        const detectionsWithDistance = obj.map(objItem => {
-            const objectWidth = objItem.bbox[2]; // assuming first object
-            const focalLength = 1000; // contoh focal length
-            const realWidth = 20; // contoh lebar asli
-            const calculatedDistance = (focalLength * realWidth) / objectWidth;
+            // Draw mesh
+            const ctx = canvasRef.current.getContext("2d");
+                
+            // Count and save distance each object
+            const detectionsWithDistance = obj.map(objItem => {
+                const objectWidth = objItem.bbox[2]; // assuming first object
+                const focalLength = 1000; // focal length
+                const realWidth = 20; //lebar asli
+                const calculatedDistance = (focalLength * realWidth) / objectWidth;
 
-            return {
-                ...objItem,
-                distance: calculatedDistance.toFixed(2)
-            };
-        });
-        // console.log(detectionsWithDistance)
-        // setDetections(detectionsWithDistance);
+                return {
+                    ...objItem,
+                    distance: calculatedDistance.toFixed(2)
+                };
+            });
 
-        // Gambar kotak pembatas dan tampilkan jarak
-        drawRect(detectionsWithDistance, ctx);
-
-        // console.log(distance);
-    }
-        
+            // Draw Stroke/line and distance
+            drawRect(detectionsWithDistance, ctx);
+        }
     };
 
     useEffect(()=>{runCoco()},[]);
 
-    const videoConstraints = {
-        facingMode: isFrontCamera ? "user" : "environment" // mengganti kamera 
-    };
     return (
         <section className="min-h-screen w-screen flex flex-col justify-center items-center gap-3">
-
             <Webcam
-        ref={webcamRef}
-        muted={true} 
-        style={{
-            position: "relative",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-        }}
-        videoConstraints={videoConstraints}
-        />
-        <canvas
-        ref={canvasRef}
-        style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 8,
-            width: 640,
-            height: 480,
-        }}
-        />
-        <button onClick={switchCamera} className='bg-blue-500 text-white px-5 py-3 rounded-lg'>Switch Camera</button>
+            ref={webcamRef}
+            muted={true} 
+            style={{
+                position: "relative",
+                marginLeft: "auto",
+                marginRight: "auto",
+                left: 0,
+                right: 0,
+                textAlign: "center",
+                zindex: 9,
+                width: 640,
+                height: 480,
+            }}
+            videoConstraints={videoConstraints}
+            />
+            <canvas
+            ref={canvasRef}
+            style={{
+                position: "absolute",
+                marginLeft: "auto",
+                marginRight: "auto",
+                left: 0,
+                right: 0,
+                textAlign: "center",
+                zindex: 8,
+                width: 640,
+                height: 480,
+            }}
+            />
+            <button onClick={switchCamera} className='bg-blue-500 text-white px-5 py-3 rounded-lg'>Switch Camera</button>
         </section>
     );
 }
