509452c5ce345efbf7d5ce786de25cbe1f9b5671
filipusarif <filipus123.gmil@gmail.com>
2024-08-11 21:21:50 +0700

    feat: debug

--- src/pages/detectPageAPI.jsx
+++ src/pages/detectPageAPI.jsx
@@ -5,7 +5,7 @@ function App() {
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
     const [detections, setDetections] = useState([]);
-    const [story, setStory] = useState('');
+    const [story, setStory] = useState([]);
     const [isFrontCamera, setIsFrontCamera] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [isActive, setIsActive] = useState(true);
@@ -16,6 +16,7 @@ function App() {
     const recognitionRef = useRef(null);
     const [isRecognizing, setIsRecognizing] = useState(false);
     const [recognizedText, setRecognizedText] = useState('');
+    const messagesEndRef = useRef(null);
 
     const switchCamera = () => {
         setIsFrontCamera(!isFrontCamera);
@@ -45,10 +46,11 @@ function App() {
     }, [isLoading]);
 
     useEffect(() => {
-        if (story) {
-            speakStory(story);
+        if (story.length > 0) {
+            speakStory(story[story.length - 1].text);
         }
     }, [story]);
+
 
     const sendFrame = async () => {
         if (!isActive) return;
@@ -66,7 +68,7 @@ function App() {
                     },
                 });
                 setDetections(response.data.detections);
-                setStory(response.data.story);
+                addChat(response.data.story, "system");
                 drawDetections(response.data.detections, context);
                 console.log(response.data.detections);
             } catch (error) {
@@ -74,6 +76,19 @@ function App() {
             }
         }, 'image/jpeg');
         requestRef.current = setTimeout(sendFrame, 15000); 
+    };
+
+    const addChat = (chat, type) => {
+        setStory(prevStory => {
+            // Check if the last message is from the same type
+            if (prevStory.length > 0 && prevStory[prevStory.length - 1].type === type) {
+                const updatedStory = [...prevStory];
+                updatedStory[updatedStory.length - 1].text = chat;
+                return updatedStory;
+            } else {
+                return [...prevStory, { text: chat, type }];
+            }
+        });
     };
 
     useEffect(() => {
@@ -124,7 +139,7 @@ function App() {
             const transcript = event.results[0][0].transcript;
             console.log('Recognized text:', transcript);
             handleCommand(transcript);
-            setRecognizedText(transcript);
+            addChat(transcript, "user");
         };
     
         recognitionRef.current.onerror = (event) => {
@@ -151,13 +166,14 @@ function App() {
                 // Contoh: Pindah ke halaman lain
                 window.location.href = 'https://example.com'; // Ganti dengan URL tujuan
             }
-        }
-        if (command.toLowerCase().startsWith('cari')) {
+        } else if (command.toLowerCase().startsWith('cari')) {
+            const action = command.slice(5).trim().toLowerCase();
+
             // Lakukan pencarian Google atau tugas lain
-            searchGoogle(command);
-        }
-        else {
+            searchGoogle(action);
+        } else {
             // chat bot
+            // chat(command)
         }
     };
 
@@ -180,6 +196,20 @@ function App() {
             speakStory('Maaf, terjadi kesalahan saat melakukan pencarian.');
         }
     };
+    const chat = async (message) => {
+        try {
+            const response = await axios.get(`http://127.0.0.1:8000/chat/`, {
+                params: {
+                    message: message
+                }
+            });
+            const { answer } = response.data;
+            speakStory(`Saya menemukan ini di Google: ${answer}.`);
+        } catch (error) {
+            console.error('Error during Google search:', error);
+            speakStory('Chat tidak dikenal');
+        }
+    };
 
     const drawDetections = (detections, context) => {
         context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
@@ -194,6 +224,11 @@ function App() {
         });
     };
 
+    useEffect(() => {
+        // Scroll to bottom whenever messages change
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [story]);
+
     return (
         <div className="App">
             {isLoading ? (
@@ -201,19 +236,41 @@ function App() {
                     <p>Loading...</p>
                 </div>
             ) : (
-                <>
-                    <h1>Object Detection</h1>
-                    <video ref={videoRef} autoPlay width="640" height="480"></video>
-                    <button onClick={switchCamera} className='bg-blue-500 text-white px-5 py-3 rounded-lg'>Switch Camera</button>
-                    <button onClick={toggleFunctions} className='bg-green-500 text-white px-5 py-3 rounded-lg'>{isActive ? 'Stop' : 'Start'}</button>
+                <main className='w-full h-screen flex justify-between items-center'>
+                    <div className='relative w-[70%] h-screen'>
+                        <video ref={videoRef} autoPlay width="200" height="120" className='absolute left-0 m-5 rounded-md'></video>
+                        //map
+                    </div>
+                    <div className='w-[30%] h-screen bg-slate-200 flex flex-col items-center justify-around'>
+                        <div>
+                            <h2>Story:</h2>
+                        </div>
+                        <main className='overflow-y-auto w-full p-5'>
+                            {story.map((message, index) => (
+                                <div
+                                    key={index}
+                                    className={`mb-2 p-3 rounded-lg shadow ${
+                                        message.type === 'system'
+                                            ? 'bg-gray-300 text-left'
+                                            : 'bg-blue-500 text-white text-right'
+                                    }`}
+                                >
+                                    <Typewriter text={message.text} delay={typingSpeed} />
+                                </div>
+                            ))}
+                            <div ref={messagesEndRef} />
+                            {isRecognizing && <p>Listening...</p>}
+                        </main>
+
+                        <div>
+                            <button onClick={switchCamera} className='bg-blue-500 text-white px-5 py-3 rounded-lg'>Switch Camera</button>
+                            <button onClick={toggleFunctions} className='bg-green-500 text-white px-5 py-3 rounded-lg'>{isActive ? 'Stop' : 'Start'}</button>
+                        </div>
+                    </div>
                     <div>
-                        <h2>Story:</h2>
-                        <Typewriter text={story} delay={typingSpeed} />
                     </div>
                     <canvas ref={canvasRef} width="640" height="480" style={{ display:'none' }}></canvas>
-                    {isRecognizing && <p>Listening...</p>}
-                    <p>Recognized Text: {recognizedText}</p> 
-                </>
+                </main>
             )}
         </div>
     );
@@ -222,6 +279,17 @@ function App() {
 const Typewriter = ({ text, delay }) => {
     const [currentText, setCurrentText] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
+
+
+    const speakStory = (text) => {
+        if (isActive) {
+            const speech = new SpeechSynthesisUtterance(text);
+            speech.lang = 'id-ID';
+            speech.pitch = 1;
+            speech.rate = 0.8;
+            window.speechSynthesis.speak(speech);
+        }
+    };
 
     useEffect(() => {
         setCurrentText('');
